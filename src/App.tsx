9de907f345d70d40e934c2fa1eab16e8ff6e245c
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-12 16:08:46 +0100

    Refactor using useContext react hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import { createContext } from "react";
 import Header from "./components/Header";
 import ScoreBoard from "./ScoreBoard";
 import Cards from "./components/Cards";
+import gearHead from "./assets/gearHead.png"
+import squanchy from "./assets/squanchy.png"
+import snuffles from "./assets/snuffles.png"
+import michael from "./assets/michael.png"
+import feratu from "./assets/vampire.png"
+import birdPerson from "./assets/birdPerson.png"
+import nebulon from "./assets/princeNebulon.png"
+import terry from "./assets/Terry.png"
+import doofusRick from "./assets/doofus rik.png"
+import presidentCurtis from "./assets/president.png"
+import abrodolphLincoler from "./assets/abraham.png"
+import rickSanchez from "./assets/rickkk.png"
+ const cards = [
+   {id:1, name:"Gear Head", image: gearHead, isClicked:false},
+   {id:2, name:"Squanchy", image: squanchy, isClicked:false},
+   {id:3, name:"Snuffles", image: snuffles, isClicked:false},
+   {id:4, name:"Krombopulos Michael", image: michael, isClicked:false},
+   {id:5, name:"Coach Feratu", image: feratu, isClicked:false},
+   {id:6, name:"Bird Person", image: birdPerson, isClicked:false},
+   {id:7, name:"Prince Nebulon", image: nebulon, isClicked:false},
+   {id:8, name:"Scarry Terry", image: terry, isClicked:false},
+   {id:9, name:"Doofus Rick", image: doofusRick, isClicked:false},
+   {id:10, name:"President Curtis", image:presidentCurtis , isClicked:false},
+   {id:11, name:"Abrodolph Lincoler", image:abrodolphLincoler , isClicked:false},
+   {id:12, name:"Rick Sanchez", image: rickSanchez, isClicked:false},
+ ]
+export const AppContext = createContext(cards);
 function App() {
   return (
     <>
-      <Header />
-      <ScoreBoard/>
-      <Cards/>
+      <AppContext.Provider value={cards}>
+        <Header />
+        <ScoreBoard />
+        <Cards />
+      </AppContext.Provider>
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,9 @@ interface CardProps {
 
 const Card = ({image, name}:CardProps) => {
   return (
-    <li className="rounded-xl border flex flex-col items-center p-2 w-[150px] h-[195px] shadow-2xl text-center">
-      <img className="h-[80%] p-1" src={image} alt="Rick" />
-      <h2 className="pt-2">{name}</h2>
+    <li className="rounded-xl border flex flex-col cursor-pointer justify-between items-center p-2 w-[150px] h-[195px] shadow-2xl text-center duration-200 hover:scale-110">
+      <img className="h-[70%]" src={image} alt="Rick" />
+      <h2 className="pt-2 text-sm font-semibold">{name}</h2>
     </li>
   );
 };
